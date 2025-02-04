e7f85fb6d1a5f4a9c1f99f22b879af68763c05e5
Ross Nguyen <xhieu94@gmail.com>
2025-02-05 00:45:37 +0700

    WIP

--- assignment/ex3_exp.sh
+++ assignment/ex3_exp.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+file_path="pre_a_ex3_exp.txt"
+
+numbers1=()
+numbers2=()
+numbers3=()
+
+while IFS= read -r line; do
+  for number in $(echo "$line" | grep -o '[0-9]\+'); do
+    numbers1+=("$number")
+  done
+done < pre_a_ex3_exp.txt
+
+while IFS= read -r line; do
+  for number in $(echo "$line" | grep -o '[0-9]\+'); do
+    numbers2+=("$number")
+  done
+done < pre_b_ex3_exp.txt
+
+while IFS= read -r line; do
+  for number in $(echo "$line" | grep -o '[0-9]\+'); do
+    numbers3+=("$number")
+  done
+done < pre_c_ex3_exp.txt
