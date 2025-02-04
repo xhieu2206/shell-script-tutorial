9ec8487efd1a77f23791d4ed8f37addd2b530c46
Ross Nguyen <xhieu94@gmail.com>
2025-02-05 01:09:22 +0700

    Done

--- assignment/ex3_exp.sh
+++ assignment/ex3_exp.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
-read_numbers_from_file() {
-  local file_path="$1"
-  local -n numbers_array="$2"
-
-  while IFS= read -r line; do
-    for number in $(grep -o '[0-9]\+' <<< "$line"); do
-      numbers_array+=("$number")
-    done
-  done < "$file_path"
-}
+file_path="pre_a_ex3_exp.txt"
 
 numbers1=()
 numbers2=()
 numbers3=()
 
-read_numbers_from_file "pre_a_ex3_exp.txt" numbers1
-read_numbers_from_file "pre_b_ex3_exp.txt" numbers2
-read_numbers_from_file "pre_c_ex3_exp.txt" numbers3
+while IFS= read -r line; do
+  for number in $(echo "$line" | grep -o '[0-9]\+'); do
+    numbers1+=("$number")
+  done
+done < pre_a_ex3_exp.txt
+
+while IFS= read -r line; do
+  for number in $(echo "$line" | grep -o '[0-9]\+'); do
+    numbers2+=("$number")
+  done
+done < pre_b_ex3_exp.txt
+
+while IFS= read -r line; do
+  for number in $(echo "$line" | grep -o '[0-9]\+'); do
+    numbers3+=("$number")
+  done
+done < pre_c_ex3_exp.txt
+
+echo "Numbers found in the file: ${numbers1[@]}"
+echo "Numbers found in the file: ${numbers2[@]}"
+echo "Numbers found in the file: ${numbers3[@]}"
+
+echo "" > final_ex3_exp.txt
+echo "FAIL: $((numbers1[0] + numbers2[0] + numbers3[0]))" > final_ex3_exp.txt
+echo >> final_ex3_exp.txt 
+echo "PASS: $((numbers1[1] + numbers2[1] + numbers3[1]))" >> final_ex3_exp.txt
+echo >> final_ex3_exp.txt 
+echo "NOTCHECK: $((numbers1[2] + numbers2[2] + numbers3[2]))" >> final_ex3_exp.txt
+echo >> final_ex3_exp.txt 
+echo "**********" >> final_ex3_exp.txt
+echo >> final_ex3_exp.txt 
+echo "SUM1: $((numbers1[0] + numbers1[1] + numbers1[2]))" >> final_ex3_exp.txt
+echo >> final_ex3_exp.txt 
+echo "SUM2: $((numbers2[0] + numbers2[1] + numbers2[2]))" >> final_ex3_exp.txt
+echo >> final_ex3_exp.txt 
+echo "SUM3: $((numbers3[0] + numbers3[1] + numbers3[2]))" >> final_ex3_exp.txt
